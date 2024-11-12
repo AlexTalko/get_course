14ebad76c29ae385b3ba6901a8b5aa77ee1a6721
air <talko.alexander@gmail.com>
2024-11-13 00:37:09 +0400

    add api.py

---
+++ main/services/api.py
@@ -0,0 +1,22 @@
+import requests
+from abc import ABC, abstractmethod
+
+from config.settings import API_KEY
+
+
+class API(ABC):
+    """API base class"""
+
+    @abstractmethod
+    def get_course(self):
+        pass
+
+
+class APICourse(API):
+    """Класс для получения курса доллара к рублю с https://api.currencylayer.com/live"""
+
+    def get_course(self):
+        response = requests.get(
+            f'https://api.currencylayer.com/live?access_key={API_KEY}&currencies=RUB')
+        if response.status_code == 200:
+            return response.json()
